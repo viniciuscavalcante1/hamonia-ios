632ca4823e21f9a70e3174285565156788bafd5a
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2025-08-30 19:53:22 -0300

    Initial Commit

---
+++ Harmonia/HarmoniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmoniaApp.swift
+//  Harmonia
+//
+//  Created by Vinícius Cavalcante on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct HarmoniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
